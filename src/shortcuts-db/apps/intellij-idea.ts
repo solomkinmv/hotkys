98492527cbe9c1016a78f0a4a422d6ada836e866
Maksym Solomkin <maksym@solomk.in>
2023-10-27 23:46:33 -0400

    Add single shortcut for IntelliJ IDEA

---
+++ src/shortcuts-db/apps/intellij-idea.ts
@@ -0,0 +1,26 @@
+import { InputApp } from "../../model/input/input-models";
+
+export const intellijIdeaShortcuts: InputApp = {
+  bundleId: "com.jetbrains.intellij",
+  name: "IntelliJ IDEA",
+  keymaps: [
+    {
+      title: "Default",
+      sections: [
+        {
+          title: "Navigation",
+          shortcuts: [
+            {
+              title: "Recent Files",
+              key: "cmd+e",
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+export const intellijIdeaEapShortcuts: InputApp = {
+  ...intellijIdeaShortcuts,
+  bundleId: "com.jetbrains.intellij-EAP",
+};
--- src/shortcuts-db/shortcuts-aggregator.ts
+++ src/shortcuts-db/shortcuts-aggregator.ts
@@ -2,5 +2,12 @@ import { safariShortcuts } from "./apps/safari";
 import { vsCodeShortcuts } from "./apps/vsCode";
 import { xcodeShortcuts } from "./apps/xcode";
 import { InputApp } from "../model/input/input-models";
+import { intellijIdeaEapShortcuts, intellijIdeaShortcuts } from "./apps/intellij-idea";
 
-export const aggregatedApps: InputApp[] = [safariShortcuts, vsCodeShortcuts, xcodeShortcuts];
+export const aggregatedApps: InputApp[] = [
+  intellijIdeaEapShortcuts,
+  intellijIdeaShortcuts,
+  safariShortcuts,
+  vsCodeShortcuts,
+  xcodeShortcuts,
+];
