22b349239aa2b4265a4f1801416b1cc9e38d5d76
Maksym Solomkin <maksym@solomk.in>
2023-10-28 10:44:33 -0400

    Disable inspections in JXA script

--- src/engine/shortcut-runner.ts
+++ src/engine/shortcut-runner.ts
@@ -4,35 +4,35 @@ import { AtomicShortcut } from "../model/internal/internal-models";
 
 // language=JavaScript
 const appleScript = `
-    // noinspection JSUnresolvedReference
+  // noinspection JSUnresolvedReference,JSUnusedLocalSymbols,JSLastCommaInObjectLiteral
 
-    function run(argv) {
-        const app = Application.currentApplication();
-        app.includeStandardAdditions = true;
+  function run(argv) {
+    const app = Application.currentApplication();
+    app.includeStandardAdditions = true;
 
-        let currentIndex = 0;
-        const targetBundleID = argv[currentIndex++];
+    let currentIndex = 0;
+    const targetBundleID = argv[currentIndex++];
 
-        // Activate the target application
-        const systemEvents = Application("System Events");
-        if (systemEvents.applicationProcesses.whose({ bundleIdentifier: targetBundleID }).length > 0) {
-            app.doShellScript("open -b " + targetBundleID);
-            delay(parseFloat(argv[currentIndex++])); // Adjust the delay as needed for the app to activate
-        }
-
-        const numberOfChords = parseInt(argv[currentIndex++]);
-        for (let i = 0; i < numberOfChords; i++) {
-            // Trigger the shortcut
-            const numberOfModifiersForChord = parseInt(argv[currentIndex++]);
-            const modifiers = [];
-            for (let j = 0; j < numberOfModifiersForChord; j++) {
-                modifiers.push(argv[currentIndex++]);
-            }
-            systemEvents.keyCode(parseInt(argv[currentIndex++]), {
-                using: modifiers,
-            });
-        }
+    // Activate the target application
+    const systemEvents = Application("System Events");
+    if (systemEvents.applicationProcesses.whose({ bundleIdentifier: targetBundleID }).length > 0) {
+      app.doShellScript("open -b " + targetBundleID);
+      delay(parseFloat(argv[currentIndex++])); // Adjust the delay as needed for the app to activate
     }
+
+    const numberOfChords = parseInt(argv[currentIndex++]);
+    for (let i = 0; i < numberOfChords; i++) {
+      // Trigger the shortcut
+      const numberOfModifiersForChord = parseInt(argv[currentIndex++]);
+      const modifiers = [];
+      for (let j = 0; j < numberOfModifiersForChord; j++) {
+        modifiers.push(argv[currentIndex++]);
+      }
+      systemEvents.keyCode(parseInt(argv[currentIndex++]), {
+        using: modifiers,
+      });
+    }
+  }
 `;
 
 export async function runShortcuts(bundleId: string, delay: number, sequence: AtomicShortcut[]) {
