a28603a24384ab54debea1164ae610eca8b9eb66
Maksym Solomkin <maksym@solomk.in>
2023-10-29 11:48:17 -0400

    Add basic display of app shortcuts

--- shortcuts-site/src/components/app/app-shortcuts.component.tsx
+++ shortcuts-site/src/components/app/app-shortcuts.component.tsx
@@ -1,6 +1,7 @@
-import { ActionFunctionArgs, ParamParseKey, Params, useLoaderData, useParams } from "react-router-dom";
-import { AppShortcuts } from "../../core/model/internal/internal-models";
-import { ShortcutsProvider, useShortcutsProvider } from "../../core/load/shortcuts-provider";
+import { useParams } from "react-router-dom";
+import { useShortcutsProvider } from "../../core/load/shortcuts-provider";
+import { SectionShortcut } from "../../core/model/internal/internal-models";
+import { modifierSymbols } from "../../core/model/internal/modifiers";
 
 // const Paths = {
 //     appShortcuts: "/apps/:bundleId",
@@ -22,9 +23,58 @@ import { ShortcutsProvider, useShortcutsProvider } from "../../core/load/shortcu
 // }
 
 export function AppShortcutsComponent() {
-    let {bundleId} = useParams();
+    let { bundleId } = useParams();
     const appShortcuts = useShortcutsProvider().getShortcutsByApp(bundleId!);
-    return (<>
-        <div>{appShortcuts?.name}</div>
-    </>);
+    return (
+        <div>
+            <h1>{appShortcuts?.name}</h1>
+            <div>{appShortcuts?.bundleId}</div>
+            {appShortcuts?.keymaps.map(keymap => {
+                return (
+                    <>
+                        <h2>{keymap.title}</h2>
+                        {keymap.sections.map(section => (
+                            <>
+                                <h3>{section.title}</h3>
+                                {section.hotkeys.map(shortcut => (
+                                    <>
+                                        <h4>{shortcut.title}</h4>
+                                        <div>{generateHotkeyText(shortcut)}</div>
+                                    </>
+                                ))}
+                            </>
+                        ))}
+                    </>
+                );
+            })}
+        </div>);
 }
+
+function generateHotkeyText(shortcut: SectionShortcut): string {
+    return shortcut.sequence
+        .map((atomicShortcut) => {
+            const modifiersText = atomicShortcut.modifiers.map((modifier) => modifierSymbols.get(modifier)).join("") ?? "";
+            return modifiersText + overrideSymbolIfPossible(atomicShortcut.base);
+        })
+        .join(" ");
+}
+
+function overrideSymbolIfPossible(base: string) {
+    if (baseKeySymbolOverride.has(base)) {
+        return baseKeySymbolOverride.get(base);
+    }
+    return base.toUpperCase();
+}
+
+const baseKeySymbolOverride: Map<string, string> = new Map([
+    ["left", "←"],
+    ["right", "→"],
+    ["up", "↑"],
+    ["down", "↓"],
+    ["pageup", "PgUp"],
+    ["pagedown", "PgDown"],
+    ["home", "Home"],
+    ["end", "End"],
+    ["tab", "⇥"],
+    ["esc", "⎋"],
+]);
