19220f9a85b645d594d5a1a08e6fcc6b9017682d
Maksym Solomkin <maksym@solomk.in>
2023-10-29 21:55:29 -0400

    Add search for apps on the main page

--- shortcuts-site/src/components/apps-list.component.tsx
+++ shortcuts-site/src/components/apps-list.component.tsx
@@ -1,16 +1,35 @@
 import { useShortcutsProvider } from "../core/load/shortcuts-provider";
 import { Link } from "react-router-dom";
-import { Flex, List, Typography } from "antd";
+import { Flex, Input, InputProps, List, Typography } from "antd";
+import { useState } from "react";
+import Fuse from "fuse.js";
 
 const { Text } = Typography;
 
 export function AppsListComponent() {
     const shortcutsProvider = useShortcutsProvider();
+    const allAppShortcuts = shortcutsProvider.getShortcuts().applications;
+    const [appShortcuts, setAppShortcuts] = useState(allAppShortcuts);
+
+    const fuse = new Fuse(allAppShortcuts, { keys: ["name"] });
+    const onChange: InputProps["onChange"] = (e) => {
+        const inputText = e.currentTarget.value;
+        if (!inputText) {
+            setAppShortcuts(allAppShortcuts);
+        } else {
+            setAppShortcuts(fuse.search(e.currentTarget.value).map(result => result.item));
+        }
+    };
+
     return (
         <Flex justify="center">
             <div>
+                <Input
+                    placeholder="Search..."
+                    onChange={onChange}
+                />
                 <List
-                    dataSource={shortcutsProvider.getShortcuts().applications}
+                    dataSource={appShortcuts}
                     renderItem={(app) => <List.Item>
                         <Link to={`/apps/${app.bundleId}`}>{app.name}</Link><Text code>{app.bundleId}</Text>
                     </List.Item>}
