9f03ae1c2036f520594ab04cb0fd7adc3e7e91b3
Maksym Solomkin <maksym@solomk.in>
2023-11-02 00:29:27 -0400

    Fix shortcut provider test

--- shortcuts-site/src/core/load/shortcuts-provider.spec.ts
+++ shortcuts-site/src/core/load/shortcuts-provider.spec.ts
@@ -1,5 +1,10 @@
 import { useShortcutsProvider } from "./shortcuts-provider";
+import { act, renderHook } from '@testing-library/react';
 
 test("Parses all shortcuts successfully", () => {
-    useShortcutsProvider().getShortcuts();
+    const { result } = renderHook(() => useShortcutsProvider());
+
+    act(() => {
+        result.current.getShortcuts();
+    });
 });
