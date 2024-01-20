7747dde891c1212bd95733df4f90d89604cdd615
Maksym Solomkin <maksym@solomk.in>
2024-01-20 12:08:42 -0500

    Update inline code component

--- shortcuts-disco-site/src/app/ui/typography.tsx
+++ shortcuts-disco-site/src/app/ui/typography.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 
-export const InlineCode = ({ children }: { children: React.ReactNode }) => (
+export const InlineCode = (
+  {
+    code,
+  }: {
+    code: string;
+  }) => (
   <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
-    {children}
+    {code}
   </code>
 );
