dca7c050667510d5ddaa360dc296404499ab2c6f
Maksym Solomkin <maksym@solomk.in>
2024-01-20 13:04:01 -0500

    Use footer based on AntDesign

--- shortcuts-disco-site/src/app/ui/footer.tsx
+++ shortcuts-disco-site/src/app/ui/footer.tsx
@@ -1,15 +1,16 @@
-import { Divider, Space } from "antd";
-import Link from "next/link";
+"use client";
+
+import { Divider, Space, Typography } from "antd";
 import React from "react";
+
+const { Text, Link } = Typography;
 
 export const Footer = () => (
   <div className="footer">
     <Divider />
     <Space split={<Divider type="vertical" />}>
-      <span className={"text-base text-gray-500"}>
-        Made by <Link href="https://solomk.in" target="_blank" color="red">Maksym
-        Solomkin</Link>
-      </span>
+      <Text type="secondary">Made by <Link href="https://solomk.in" target="_blank" color="red">Maksym
+        Solomkin</Link></Text>
       <Link href="https://github.com/solomkinmv/shortcuts-disco/issues" target="_blank">Report issues or make a
         request</Link>
     </Space>
