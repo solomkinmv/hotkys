265e2d952026962eaae5b324615c2e33c0c49bae
Maksym Solomkin <maksym@solomk.in>
2024-01-20 15:58:00 -0500

    Fix repo link

--- shortcuts-disco-site/src/app/about/page.tsx
+++ shortcuts-disco-site/src/app/about/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
         <Text>Note: currently support only macOs. Please vote for this feature <Link
           href="https://github.com/solomkinmv/shortcuts-disco/issues/2" target="_blank">here</Link></Text>
         <Title level={3}>Contribution</Title>
-        <Text>Create PR with shortcuts in <Text code>shortcuts-data</Text> on <Link href="https://github.com/solomkinmv/shortcuts-disco/tree/main/shortcuts-site/shortcuts-data"
+        <Text>Create PR with shortcuts in <Text code>shortcuts-data</Text> on <Link href="https://github.com/solomkinmv/shortcuts-disco/tree/main/shortcuts-disco-site/shortcuts-data"
                                                                                     target="_blank">GitHub</Link>.</Text>
         <Text>Include schema for each application <Text code>{code}</Text> as a first JSON
           property.</Text>
