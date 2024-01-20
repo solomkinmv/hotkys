84a69d9f972c7879c7324b79f638c76b7c6d8773
Maksym Solomkin <maksym@solomk.in>
2024-01-20 16:30:28 -0500

    Update test to check raw shortcuts data source

--- shortcuts-disco-site/__tests__/helpers.ts
+++ shortcuts-disco-site/__tests__/helpers.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import fs from "fs";
-import { AllApps } from "@/lib/model/input/input-models";
+import { AllApps, InputApp } from "@/lib/model/input/input-models";
 
 export function parseKeyCodes(): Map<string, string> {
   const filePath = path.resolve(__dirname, "../public/data", "key-codes.json");
@@ -9,8 +9,18 @@ export function parseKeyCodes(): Map<string, string> {
   return new Map<string, string>(keyCodesObject.keyCodes);
 }
 
-export function parseAllApps() {
-  const filePath = path.resolve(__dirname, "../public/data", "combined-apps.json");
-  const fileContents = fs.readFileSync(filePath, "utf8");
-  return JSON.parse(fileContents) as AllApps;
+export function parseAllApps(): AllApps {
+  const dirPath = path.resolve(__dirname, "../shortcuts-data");
+  const fileNames = fs.readdirSync(dirPath);
+  const jsonFileNames = fileNames.filter(fileName => fileName.endsWith('.json'));
+
+  const allApps: InputApp[] = jsonFileNames.flatMap(fileName => {
+    const filePath = path.resolve(dirPath, fileName);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(fileContents) as InputApp[];
+  });
+
+  return {
+    list: allApps,
+  };
 }
