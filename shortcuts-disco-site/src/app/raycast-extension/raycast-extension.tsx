1cc99ace493d13cc953cdbb3e3fba9c53873a583
Maksym Solomkin <maksym@solomk.in>
2024-01-24 19:45:36 -0500

    Fix Raycast image warning

--- shortcuts-disco-site/src/app/raycast-extension/raycast-extension.tsx
+++ shortcuts-disco-site/src/app/raycast-extension/raycast-extension.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import {Image, Space, Typography} from 'antd';
+import { Image, Space, Typography } from "antd";
 
-const {Title, Text, Link} = Typography;
+const { Title, Text, Link } = Typography;
 
 export default function RaycastExtensionContent() {
   return (
@@ -11,10 +11,11 @@ export default function RaycastExtensionContent() {
         <Title>RayCast Extension: Shortcuts Search</Title>
         <Image.PreviewGroup>
           <Space direction="horizontal" wrap={true}>
-            <Image alt="List of all aps in Raycast extension" width={380} src="media/shortcuts-search-1.png"/>
-            <Image alt="App search in Raycast extension" width={380} src="media/shortcuts-search-2.png"/>
-            <Image alt="Application specific shortcuts in Raycast extension" width={380} src="media/shortcuts-search-3.png"/>
-            <Image alt="Shortcut sarch in Raycast extension" width={380} src="media/shortcuts-search-4.png"/>
+            <Image alt="List of all aps in Raycast extension" width={380} src="media/shortcuts-search-1.png" />
+            <Image alt="App search in Raycast extension" width={380} src="media/shortcuts-search-2.png" />
+            <Image alt="Application specific shortcuts in Raycast extension" width={380}
+                   src="media/shortcuts-search-3.png" />
+            <Image alt="Shortcut sarch in Raycast extension" width={380} src="media/shortcuts-search-4.png" />
           </Space>
         </Image.PreviewGroup>
         <Text><Link href="https://www.raycast.com" target="_blank">RayCast</Link> is a productivity tool,
@@ -26,16 +27,16 @@ export default function RaycastExtensionContent() {
           Store</Link>.</Text>
         <a title="Install shortcuts-search Raycast Extension"
            href="https://www.raycast.com/solomkinmv/shortcuts-search"
+           target={"_blank"}
            style={{
              display: "flex",
-             justifyContent: "center"
+             justifyContent: "center",
            }}>
-          <img
+          <Image
+            preview={false}
             src="https://www.raycast.com/solomkinmv/shortcuts-search/install_button@2x.png?v=1.1"
-            height="64"
-            alt="" style={{
-            height: "64px"
-          }}/>
+            alt="Install Shortcuts Disco Raycas extension" style={{ height: 64, width: 256 }}
+          />
         </a>
       </Space>
     </div>
