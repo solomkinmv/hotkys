c9f474d8bdf57bb6422bae0719d313c40b1c54db
Maksym Solomkin <maksym@solomk.in>
2024-04-08 00:54:30 -0400

    Fix build

--- shortcuts-disco-site/src/app/apps/[slug]/[keymap]/page.tsx
+++ shortcuts-disco-site/src/app/apps/[slug]/[keymap]/page.tsx
@@ -1,4 +1,4 @@
-import {getAppShortcutsBySlug} from "@/lib/shortcuts";
+import {getAllShortcuts, getAppShortcutsBySlug} from "@/lib/shortcuts";
 import {notFound} from "next/navigation";
 import {keymapMatchesTitle, serializeKeymap} from "@/lib/model/keymap-utils";
 import {AppDetails} from "@/app/apps/[slug]/[keymap]/app-details";
@@ -8,18 +8,16 @@ interface Props {
     params: { slug: string, keymap: string };
 }
 
-export async function generateStaticParams({
-                                               params: {slug},
-                                           }: {
-    params: { slug: string }
-}) {
-    return (getAppShortcutsBySlug(slug)?.keymaps ?? [])
-        .map(keymap => ({
+export async function generateStaticParams() {
+    return getAllShortcuts().applications
+        .flatMap(app => app.keymaps.map(keymap => ({
+            slug: app.slug,
             keymap: serializeKeymap(keymap)
-        }));
+        })));
 }
 
 export default function SingleApplicationPage({params}: Props) {
+    console.log("building page", params);
     const appShortcuts = getAppShortcutsBySlug(params.slug) || notFound();
     const keymap = findKeymap(appShortcuts, params.keymap) || notFound();
 
