b5486ecc948fc096ccb53b7e2af209f24662f89e
Maksym Solomkin <maksym@solomk.in>
2024-04-30 22:02:13 -0400

    Update links to GitHub repo

--- shortcuts-disco-site/src/app/about/page.tsx
+++ shortcuts-disco-site/src/app/about/page.tsx
@@ -14,16 +14,16 @@ export default function About() {
             <Paragraph>Shortcuts database for different applications.</Paragraph>
             <Paragraph>
                 Note: currently support only macOs. Please vote for this feature <Link
-                href="https://github.com/solomkinmv/shortcuts-disco/issues/2" target="_blank">here</Link>
+                href="https://github.com/solomkinmv/hotkys/issues/2" target="_blank">here</Link>
             </Paragraph>
             <h3>Contribution</h3>
             <Paragraph>
                 Create PR with shortcuts in <kbd>shortcuts-data</kbd> on <Link
-                href="https://github.com/solomkinmv/shortcuts-disco/tree/main/shortcuts-disco-site/shortcuts-data"
+                href="https://github.com/solomkinmv/hotkys/tree/main/shortcuts-disco-site/shortcuts-data"
                 target="_blank">GitHub</Link>.</Paragraph>
             <Paragraph>Include schema for each application <kbd>{code}</kbd> as a first JSON
                 property.</Paragraph>
-            <Paragraph>See full contribution guide on <Link href="https://github.com/solomkinmv/shortcuts-disco"
+            <Paragraph>See full contribution guide on <Link href="https://github.com/solomkinmv/hotkys"
                                                             target="_blank">GitHub</Link></Paragraph>
         </section>
     );
