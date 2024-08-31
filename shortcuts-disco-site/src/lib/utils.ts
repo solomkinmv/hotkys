36359e88e9e643cd994014e07df6932977e47b47
Thor Galle <thorgalle@gmail.com>
2024-08-31 11:49:55 +0300

    Use chokidar for shortcut file watching during development. Rewrite combine-apps in Node.js.
    The prettifier now also attempts to canonicalize invalid alternative modifier and key names, next to only reordering them.
    
    This improves the previous solution by using
    - per-file prettifying and writing
    - validation before writing
    - removing non-Node.js dependencies of bash, jq and fswatch

--- shortcuts-disco-site/src/lib/utils.ts
+++ shortcuts-disco-site/src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import path from "path"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const rootFolder = path.resolve(__dirname, '../..')
+export const dataFolder = path.resolve(rootFolder, 'shortcuts-data')
