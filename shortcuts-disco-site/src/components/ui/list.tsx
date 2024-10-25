75f24e01a517bfe21b9a910305e4b252216049ef
Maksym Solomkin <maksym@solomk.in>
2024-10-25 00:42:53 -0400

    Add arrow navigation to select highlight shortcut

--- shortcuts-disco-site/src/components/ui/list.tsx
+++ shortcuts-disco-site/src/components/ui/list.tsx
@@ -1,35 +1,40 @@
-import clsx from "clsx";
 import Link from "next/link";
 import React, { forwardRef } from "react";
+import { cn } from "@/lib/utils";
 
 export const LinkableListItem = forwardRef<
-  HTMLAnchorElement,
+  HTMLDivElement,
   {
     children: React.ReactNode;
     to: string;
     selected: boolean;
   }
 >(({ children, to, selected }, ref) => (
-  <Link href={to} passHref className="no-underline" ref={ref}>
-    <ListItem selected={selected}>{children}</ListItem>
+  <Link href={to} passHref className="no-underline">
+    <ListItem selected={selected} ref={ref}>
+      {children}
+    </ListItem>
   </Link>
 ));
 
 LinkableListItem.displayName = "LinkableListItem";
 
-export const ListItem = ({
-  children,
-  selected,
-}: {
-  children: React.ReactNode;
-  selected: boolean;
-}) => (
+export const ListItem = forwardRef<
+  HTMLDivElement,
+  {
+    children: React.ReactNode;
+    selected: boolean;
+  }
+>(({ children, selected }, ref) => (
   <div
-    className={clsx(
+    ref={ref}
+    className={cn(
       "flex cursor-pointer items-center justify-between border-b p-2 prose-sm last:border-0 hover:bg-slate-100/50",
       { "outline outline-2 outline-offset-1 rounded-md": selected },
     )}
   >
     {children}
   </div>
-);
+));
+
+ListItem.displayName = "ListItem";
